289080083f3683da7aa73b7eee6cdf65878226b6
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-12-23 21:44:39 +0100

    //

---
+++ SecondTask/Assets/CubeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+namespace Assets
+{
+  public  class CubeController:MonoBehaviour
+    {
+        [SerializeField] GameObject cube1;
+        [SerializeField] Button button;
+
+        void Start()
+        {
+            button.gameObject.SetActive(false);
+            button.onClick.AddListener(() => Restart());
+        }
+
+
+           void OnCollisionEnter(Collision col)
+            {
+            if (col.gameObject.name == "CubeTwo")
+            {
+                col.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                cube1.GetComponent<Renderer>().material.color = Color.yellow;
+                button.gameObject.SetActive(true);
+            }
+            }
+
+             void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Vector3 position = this.transform.position;
+                position.z++;
+                this.transform.position = position;
+            }
+        }
+
+   
+        void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
